eb4948864ea9a02ec9bca79f8d09ae6e71c03bda
Marco Reyes <marcoreyes1518@gmail.com>
2014-09-26 12:22:47 -0600

    Proyecto desde TFS

---
+++ Boletas/Boletas/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Boletas
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
